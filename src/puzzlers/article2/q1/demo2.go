4526c9baa3224c21b0da86ecca1c382e51c6bb54
guoguo <noobwu@126.com>
2021-07-01 22:55:06 +0800

    Update demo2.go

--- src/puzzlers/article2/q1/demo2.go
+++ src/puzzlers/article2/q1/demo2.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"flag"
+	"flag" //首先，Go 语言标准库中有一个代码包专门用于接收和解析命令参数。这个代码包的名字叫flag。
 	"fmt"
 )
 
 var name string
 
 func init() {
+
 	flag.StringVar(&name, "name", "everyone", "The greeting object.")
 }
 
