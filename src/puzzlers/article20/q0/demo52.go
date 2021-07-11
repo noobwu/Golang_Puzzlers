4f50ed0ef089df9afa68ceec229eeb919f8f74c9
guoguo <noobwu@126.com>
2021-07-12 03:25:04 +0800

    description
    description

--- src/puzzlers/article20/q0/demo52.go
+++ src/puzzlers/article20/q0/demo52.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"errors"
-	"flag"
+	"flag" //首先，Go 语言标准库中有一个代码包专门用于接收和解析命令参数。这个代码包的名字叫flag。
 	"fmt"
 )
 
 var name string
 
 func init() {
+	/*
+		函数flag.StringVar接受 4 个参数
+			第 1 个参数是用于存储该命令参数值的地址，具体到这里就是在前面声明的变量name的地址了，由表达式&name表示。
+			第 2 个参数是为了指定该命令参数的名称，这里是name。
+			第 3 个参数是为了指定在未追加该命令参数时的默认值，这里是everyone。
+			至于第 4 个函数参数，即是该命令参数的简短说明了，这在打印命令说明时会用到。
+	*/
 	flag.StringVar(&name, "name", "everyone", "The greeting object.")
 }
 
--- src/puzzlers/article20/q0/demo52_test.go
+++ src/puzzlers/article20/q0/demo52_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+/*
+这里我给出的典型回答是下面三个内容。
+	对于功能测试函数来说，其名称必须以Test为前缀，并且参数列表中只应有一个*testing.T类型的参数声明。
+	对于性能测试函数来说，其名称必须以Benchmark为前缀，并且唯一参数的类型必须是*testing.B类型的。
+	对于示例测试函数来说，其名称必须以Example为前缀，但对函数的参数列表没有强制规定。
+*/
+
 func TestHello(t *testing.T) {
 	var name string
 	greeting, err := hello(name)
@@ -34,6 +41,7 @@ func TestHello(t *testing.T) {
 	t.Logf("The expected greeting is %q.\n", expected)
 }
 
+//函数名称不符合规则
 func testIntroduce(t *testing.T) { // 请注意这个测试函数的名称。
 	intro := introduce()
 	expected := "Welcome to my Golang column."
