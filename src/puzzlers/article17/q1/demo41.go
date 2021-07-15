a8fd65a2009c73bb207c39b0f4d14b07f85917d6
guoguo <noobwu@126.com>
2021-07-15 21:45:02 +0800

    Update demo41.go

--- src/puzzlers/article17/q1/demo41.go
+++ src/puzzlers/article17/q1/demo41.go
@@ -4,17 +4,17 @@ import "fmt"
 
 func main() {
 	// 示例1。
-	numbers1 := []int{1, 2, 3, 4, 5, 6}
+	numbers1 := []int{1, 2, 3, 4, 5, 6} //切片
 	for i := range numbers1 {
 		if i == 3 {
 			numbers1[i] |= i
 		}
 	}
-	fmt.Println(numbers1)
+	fmt.Println(numbers1) //[1 2 3 7 5 6]
 	fmt.Println()
 
 	// 示例2。
-	numbers2 := [...]int{1, 2, 3, 4, 5, 6}
+	numbers2 := [...]int{1, 2, 3, 4, 5, 6} //数组是值类型
 	maxIndex2 := len(numbers2) - 1
 	for i, e := range numbers2 {
 		if i == maxIndex2 {
@@ -23,11 +23,11 @@ func main() {
 			numbers2[i+1] += e
 		}
 	}
-	fmt.Println(numbers2)
+	fmt.Println(numbers2) //[7 3 5 7 9 11]
 	fmt.Println()
 
 	// 示例3。
-	numbers3 := []int{1, 2, 3, 4, 5, 6}
+	numbers3 := []int{1, 2, 3, 4, 5, 6} //切片是引用类型
 	maxIndex3 := len(numbers2) - 1
 	for i, e := range numbers3 {
 		if i == maxIndex3 {
@@ -36,5 +36,5 @@ func main() {
 			numbers3[i+1] += e
 		}
 	}
-	fmt.Println(numbers3)
+	fmt.Println(numbers3) //[22 3 6 10 15 21]
 }
